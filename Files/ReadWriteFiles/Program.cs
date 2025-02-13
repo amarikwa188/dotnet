3502a4b2562b3c55170f1b77d3b0266e8dab3bf3
Amarikwa <amarikwa188@gmail.com>
2025-02-13 10:26:59 +0100

    read and write files

--- Files/ReadWriteFiles/Program.cs
+++ Files/ReadWriteFiles/Program.cs
@@ -6,14 +6,22 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-    File.WriteAllText(filename, "This is a a text file.");
+    File.WriteAllText(filename, "This is application programming.\n");
 }
 
 // TODO 3: AppendAllText adds text to an existing file
+File.AppendAllText(filename, "300 Level Second Semester Course.\n");
 
 
 // TODO 4: A FileStream can be opened and written to until closed
+using (StreamWriter sw = File.AppendText(filename)){
+    sw.WriteLine("Ashley");
+    sw.WriteLine("Amarikwa");
+    sw.WriteLine("9400");
+}
 
 
 // TODO 2: ReadAllText reads all the content from a file
-
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
